cbbf5ad5926c8d719150c0ed959220fc8a62d408
kaibs <kai.b.schneider@gmail.com>
2023-05-24 23:07:16 +0200

    fixed own cfgs

--- keyboards/bm60hsrgb_iso/rev1/keymaps/custom/keymap.c
+++ keyboards/bm60hsrgb_iso/rev1/keymaps/custom/keymap.c
@@ -30,14 +30,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_60_iso_arrow(
-        KC_GESC,        DE_1,        DE_2,        DE_3,  DE_4,  DE_5,    DE_6,  DE_7,  DE_8,    DE_9,    DE_0,           DE_SS, DE_ACUT, KC_BSPC,
+        QK_GESC,        DE_1,        DE_2,        DE_3,  DE_4,  DE_5,    DE_6,  DE_7,  DE_8,    DE_9,    DE_0,           DE_SS, DE_ACUT, KC_BSPC,
         KC_TAB,  LT(2, DE_Q),        DE_W,        DE_E,  DE_R,  DE_T,    DE_Z,  DE_U,  DE_I,    DE_O,    DE_P,         DE_UDIA, DE_PLUS,
         KC_CAPS, LT(3, DE_A), LT(4, DE_S), LT(5, DE_D),  DE_F,  DE_G,    DE_H,  DE_J,  DE_K,    DE_L, DE_ODIA,         DE_ADIA, DE_HASH,  KC_ENT,
         KC_LSFT,     DE_LABK,        DE_Y,        DE_X,  DE_C,  DE_V,    DE_B,  DE_N,  DE_M, DE_COMM,  DE_DOT, RSFT_T(DE_MINS),   KC_UP,   TG(6),
         KC_LCTL,     KC_LGUI,     KC_LALT,                        KC_SPC,                    KC_RALT,   MO(1),         KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_60_iso_arrow(
-        RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        QK_RBT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_TOG, KC_MPRV, KC_MPLY, KC_MNXT,
         _______, DE_CIRC, DE_DEG, RGB_M_P, RGB_M_B, RGB_SPD, RGB_SPI, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______
     ),
     [6] = LAYOUT_60_iso_arrow(
-        KC_GESC,    DE_1,  DE_2,  DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,       DE_9,    DE_0,           DE_SS, DE_ACUT, KC_BSPC,
+        QK_GESC,    DE_1,  DE_2,  DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,       DE_9,    DE_0,           DE_SS, DE_ACUT, KC_BSPC,
         KC_TAB,     DE_Q,  DE_W,  DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,       DE_O,    DE_P,         DE_UDIA, DE_PLUS,
         KC_CAPS,    DE_A,  DE_S,  DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,       DE_L, DE_ODIA,         DE_ADIA, DE_HASH,  KC_ENT,
         KC_LSFT, DE_LABK,  DE_Y,  DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM,  DE_DOT, RSFT_T(DE_MINS),   KC_UP,   TG(6),
